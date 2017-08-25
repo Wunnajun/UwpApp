98673b06993b96f311c7c5698c75555f11894f60
Wunnajun <chompoo.cp007@gmail.com>
2017-08-25 15:59:06 +0700

    calculator

---
+++ CalcularMY/CalcularMY/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace CalcularMY
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_seven = new System.Windows.Forms.Button();
+            this.button_eight = new System.Windows.Forms.Button();
+            this.button_four = new System.Windows.Forms.Button();
+            this.CE = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button_nine = new System.Windows.Forms.Button();
+            this.button_five = new System.Windows.Forms.Button();
+            this.button_six = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button_two = new System.Windows.Forms.Button();
+            this.button_one = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button_three = new System.Windows.Forms.Button();
+            this.total = new System.Windows.Forms.Button();
+            this.botton_zero = new System.Windows.Forms.Button();
+            this.dot = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.TextBox();
+            this.percent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_seven
+            // 
+            this.button_seven.Location = new System.Drawing.Point(25, 109);
+            this.button_seven.Name = "button_seven";
+            this.button_seven.Size = new System.Drawing.Size(45, 45);
+            this.button_seven.TabIndex = 0;
+            this.button_seven.Text = "7";
+            this.button_seven.UseVisualStyleBackColor = true;
+            this.button_seven.Click += new System.EventHandler(this.button_seven_Click);
+            // 
+            // button_eight
+            // 
+            this.button_eight.Location = new System.Drawing.Point(76, 109);
+            this.button_eight.Name = "button_eight";
+            this.button_eight.Size = new System.Drawing.Size(45, 45);
+            this.button_eight.TabIndex = 1;
+            this.button_eight.Text = "8";
+            this.button_eight.UseVisualStyleBackColor = true;
+            this.button_eight.Click += new System.EventHandler(this.button_eight_Click);
+            // 
+            // button_four
+            // 
+            this.button_four.Location = new System.Drawing.Point(25, 160);
+            this.button_four.Name = "button_four";
+            this.button_four.Size = new System.Drawing.Size(45, 45);
+            this.button_four.TabIndex = 2;
+            this.button_four.Tag = "";
+            this.button_four.Text = "4";
+            this.button_four.UseVisualStyleBackColor = true;
+            this.button_four.Click += new System.EventHandler(this.button_four_Click);
+            // 
+            // CE
+            // 
+            this.CE.Location = new System.Drawing.Point(229, 109);
+            this.CE.Name = "CE";
+            this.CE.Size = new System.Drawing.Size(45, 45);
+            this.CE.TabIndex = 3;
+            this.CE.Text = "CE";
+            this.CE.UseVisualStyleBackColor = true;
+            this.CE.Click += new System.EventHandler(this.CE_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(178, 108);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(45, 45);
+            this.button5.TabIndex = 4;
+            this.button5.Tag = "";
+            this.button5.Text = "/";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Operater_click);
+            // 
+            // button_nine
+            // 
+            this.button_nine.Location = new System.Drawing.Point(127, 108);
+            this.button_nine.Name = "button_nine";
+            this.button_nine.Size = new System.Drawing.Size(45, 45);
+            this.button_nine.TabIndex = 5;
+            this.button_nine.Text = "9";
+            this.button_nine.UseVisualStyleBackColor = true;
+            this.button_nine.Click += new System.EventHandler(this.button_nine_Click);
+            // 
+            // button_five
+            // 
+            this.button_five.Location = new System.Drawing.Point(76, 160);
+            this.button_five.Name = "button_five";
+            this.button_five.Size = new System.Drawing.Size(45, 45);
+            this.button_five.TabIndex = 6;
+            this.button_five.Text = "5";
+            this.button_five.UseVisualStyleBackColor = true;
+            this.button_five.Click += new System.EventHandler(this.button_five_Click);
+            // 
+            // button_six
+            // 
+            this.button_six.Location = new System.Drawing.Point(127, 159);
+            this.button_six.Name = "button_six";
+            this.button_six.Size = new System.Drawing.Size(45, 45);
+            this.button_six.TabIndex = 7;
+            this.button_six.Text = "6";
+            this.button_six.UseVisualStyleBackColor = true;
+            this.button_six.Click += new System.EventHandler(this.button_six_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(178, 160);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(45, 45);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "*";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Operater_click);
+            // 
+            // button_two
+            // 
+            this.button_two.Location = new System.Drawing.Point(76, 211);
+            this.button_two.Name = "button_two";
+            this.button_two.Size = new System.Drawing.Size(45, 45);
+            this.button_two.TabIndex = 10;
+            this.button_two.Text = "2";
+            this.button_two.UseVisualStyleBackColor = true;
+            this.button_two.Click += new System.EventHandler(this.button_two_Click);
+            // 
+            // button_one
+            // 
+            this.button_one.Location = new System.Drawing.Point(25, 211);
+            this.button_one.Name = "button_one";
+            this.button_one.Size = new System.Drawing.Size(45, 45);
+            this.button_one.TabIndex = 11;
+            this.button_one.Text = "1";
+            this.button_one.UseVisualStyleBackColor = true;
+            this.button_one.Click += new System.EventHandler(this.button_one_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(178, 211);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(45, 45);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "-";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.Operater_click);
+            // 
+            // button_three
+            // 
+            this.button_three.Location = new System.Drawing.Point(127, 210);
+            this.button_three.Name = "button_three";
+            this.button_three.Size = new System.Drawing.Size(45, 45);
+            this.button_three.TabIndex = 13;
+            this.button_three.Text = "3";
+            this.button_three.UseVisualStyleBackColor = true;
+            this.button_three.Click += new System.EventHandler(this.button_three_Click);
+            // 
+            // total
+            // 
+            this.total.Location = new System.Drawing.Point(229, 210);
+            this.total.Name = "total";
+            this.total.Size = new System.Drawing.Size(45, 97);
+            this.total.TabIndex = 14;
+            this.total.Text = "=";
+            this.total.UseVisualStyleBackColor = true;
+            this.total.Click += new System.EventHandler(this.total_click);
+            // 
+            // botton_zero
+            // 
+            this.botton_zero.Location = new System.Drawing.Point(25, 262);
+            this.botton_zero.Name = "botton_zero";
+            this.botton_zero.Size = new System.Drawing.Size(96, 45);
+            this.botton_zero.TabIndex = 15;
+            this.botton_zero.Text = "0";
+            this.botton_zero.UseVisualStyleBackColor = true;
+            this.botton_zero.Click += new System.EventHandler(this.botton_zero_Click);
+            // 
+            // dot
+            // 
+            this.dot.Location = new System.Drawing.Point(127, 262);
+            this.dot.Name = "dot";
+            this.dot.Size = new System.Drawing.Size(45, 45);
+            this.dot.TabIndex = 16;
+            this.dot.Text = ".";
+            this.dot.UseVisualStyleBackColor = true;
+            this.dot.Click += new System.EventHandler(this.dot_Click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(178, 262);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(45, 45);
+            this.button18.TabIndex = 17;
+            this.button18.Text = "+";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.Operater_click);
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(25, 41);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(249, 44);
+            this.result.TabIndex = 18;
+            this.result.Text = "0";
+            this.result.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // percent
+            // 
+            this.percent.Location = new System.Drawing.Point(229, 160);
+            this.percent.Name = "percent";
+            this.percent.Size = new System.Drawing.Size(45, 45);
+            this.percent.TabIndex = 19;
+            this.percent.Text = "%";
+            this.percent.UseVisualStyleBackColor = true;
+            this.percent.Click += new System.EventHandler(this.percent_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 320);
+            this.Controls.Add(this.percent);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.dot);
+            this.Controls.Add(this.botton_zero);
+            this.Controls.Add(this.total);
+            this.Controls.Add(this.button_three);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button_one);
+            this.Controls.Add(this.button_two);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button_six);
+            this.Controls.Add(this.button_five);
+            this.Controls.Add(this.button_nine);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.CE);
+            this.Controls.Add(this.button_four);
+            this.Controls.Add(this.button_eight);
+            this.Controls.Add(this.button_seven);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_seven;
+        private System.Windows.Forms.Button button_eight;
+        private System.Windows.Forms.Button button_four;
+        private System.Windows.Forms.Button CE;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button_nine;
+        private System.Windows.Forms.Button button_five;
+        private System.Windows.Forms.Button button_six;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button_two;
+        private System.Windows.Forms.Button button_one;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button_three;
+        private System.Windows.Forms.Button total;
+        private System.Windows.Forms.Button botton_zero;
+        private System.Windows.Forms.Button dot;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button percent;
+    }
+}
+
---
+++ CalcularMY/CalcularMY/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CalcularMY
+{
+    public partial class Form1 : Form
+    {
+        string input = string.Empty;
+        string operate1 = string.Empty;
+        string operate2 = string.Empty;
+        double value = 0;
+        double num1, num2;
+        string operation = " "; //string for operation
+        bool operation_pressed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void botton_zero_Click(object sender, EventArgs e)
+        {
+            if ((result.Text == "0") || (operation_pressed))
+                result.Clear();
+            operation_pressed = false;
+            //input += "1";
+            Button b = (Button)sender;
+
+            result.Text += b.Text;
+        }
+
+        private void button_one_Click(object sender, EventArgs e)
+        {
+            if ((result.Text == "0") || (operation_pressed))
+                 result.Clear();
+            operation_pressed = false;
+            //input += "1";
+            Button b = (Button)sender;
+
+            result.Text += b.Text;
+
+        }
+        private void button_two_Click(object sender, EventArgs e)
+        {
+            if ((result.Text == "0") || (operation_pressed))
+                result.Clear();
+            operation_pressed = false;
+            //input += "2";
+            Button b = (Button)sender;
+
+            result.Text += b.Text;
+        }
+        private void button_three_Click(object sender, EventArgs e)
+        {
+            if ((result.Text == "0") || (operation_pressed))
+                result.Clear();
+            operation_pressed = false;
+            //input += "3";
+            Button b = (Button)sender;
+
+            result.Text += b.Text;
+        }
+        private void button_four_Click(object sender, EventArgs e)
+        {
+            if ((result.Text == "0") || (operation_pressed))
+                result.Clear();
+            operation_pressed = false;
+            //input += "4";
+            Button b = (Button)sender;
+
+            result.Text += b.Text;
+        }
+
+        private void button_five_Click(object sender, EventArgs e)
+        {
+            if ((result.Text == "0") || (operation_pressed))
+                result.Clear();
+            //input += "5";
+
+            operation_pressed = false;
+            Button b = (Button)sender;
+
+            result.Text += b.Text;
+        }
+
+        private void button_six_Click(object sender, EventArgs e)
+        {
+            if ((result.Text == "0") || (operation_pressed))
+                result.Clear();
+            operation_pressed = false;
+           // input += "6";
+            Button b = (Button)sender;
+
+            result.Text += b.Text;
+        }
+
+        private void button_seven_Click(object sender, EventArgs e)
+        {
+            if ((result.Text == "0") || (operation_pressed))
+                result.Clear();
+            //input += "7";
+            operation_pressed = false;
+            Button b = (Button)sender;
+
+            result.Text += b.Text;
+        }
+
+        private void button_eight_Click(object sender, EventArgs e)
+        {
+
+            if ((result.Text == "0") || (operation_pressed))
+                result.Clear();
+
+            operation_pressed = false; 
+            // input += "8";
+            Button b = (Button)sender;
+
+            result.Text += b.Text;
+        }
+
+        private void button_nine_Click(object sender, EventArgs e)
+        {
+            if ((result.Text == "0") || (operation_pressed))
+                result.Clear();
+            operation_pressed = false;
+            //input += "9";
+            Button b = (Button)sender;
+
+            result.Text += b.Text;
+        }
+        private void dot_Click(object sender, EventArgs e)
+        {
+            if ((result.Text == "0") || (operation_pressed))
+                result.Clear();
+            operation_pressed = false;
+            // input += ".";
+            Button b = (Button)sender;
+
+            result.Text += b.Text;
+        }
+
+
+
+        // private void botton_Click(object sender, EventArgs e)
+        //{
+        //if ((result.Text == "0") || (operation_pressed))
+        // result.Clear();
+        //                Button b = (Button)sender;
+
+        // result.Text += b.Text;
+
+        // }
+        
+
+        
+
+        private void CE_Click(object sender, EventArgs e)   //buttom CE
+        {
+            result.Text = "0";    //if click it show 0
+        }
+       
+            private void Operater_click(object sender, EventArgs e)
+            {
+                Button b = (Button)sender;
+                operation = b.Text;
+                value = Double.Parse(result.Text);
+                operation_pressed = true;
+            }
+
+            private void total_click(object sender, EventArgs e)  //+ - * %
+            {
+                switch (operation)
+                {
+                    case "+":
+                        result.Text = (value + double.Parse(result.Text)).ToString();
+                        break;
+                    case "-":
+                        result.Text = (value - double.Parse(result.Text)).ToString();
+                        break;
+                    case "*":
+                        result.Text = (value * double.Parse(result.Text)).ToString();
+                        break;
+                    case "/":
+                        result.Text = (value / double.Parse(result.Text)).ToString();
+                        break;
+
+
+                    default:
+                        break;
+
+                }
+            operation_pressed = false;
+        }
+
+       
+
+        /* private void Operater_click(object sender, EventArgs e)
+{
+    Button b = (Button)sender;
+    operation = b.Text;
+    value = Double.Parse(result.Text);
+    operation_pressed = true;
+}
+
+private void total_click(object sender, EventArgs e)  //+ - * %
+{
+    switch (operation)
+    {
+        case "+":
+            result.Text = (value + double.Parse(result.Text)).ToString();
+            break;
+        case "-":
+            result.Text = (value - double.Parse(result.Text)).ToString();
+            break;
+        case "*":
+            result.Text = (value * double.Parse(result.Text)).ToString();
+            break;
+        case "/":
+            result.Text = (value / double.Parse(result.Text)).ToString();
+            break;
+
+
+        default:
+            break;
+
+    }
+    operation_pressed = false;
+}*/
+
+
+        private void percent_Click(object sender, EventArgs e)
+        {
+            switch (operation)
+            {
+                case "+":
+                    result.Text = (value + double.Parse(result.Text) / 100 * value).ToString();
+                    break;
+                case "-":
+                    result.Text = (value - double.Parse(result.Text) / 100 * value).ToString();
+                    break;
+
+            }
+
+        }
+    }
+}
+    
+
